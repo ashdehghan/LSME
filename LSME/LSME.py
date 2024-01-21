a50657c5fd35c72d278bc832a732bb963cbe598a
Ash D <ash.dehghan@gmail.com>
2024-01-21 00:18:16 +0000

    Working on the sub-graph adjacency matrix sampling.

--- LSME/LSME.py
+++ LSME/LSME.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import pandas as pd
 import networkx as nx
 from tqdm import tqdm
 import matplotlib.pyplot as plt
@@ -18,12 +19,23 @@ class LSME:
 
 	def build_embeddings(self, emb_dim, receptive_field, sample_size):
 		A_set = []
+		node_ids = []
+		node_labels = []
 		for x in tqdm(list(self.G.nodes)):
-			nb, nb_list = self.get_neighborhood_of_x(x, receptive_field)
+			node_ids.append(x)
+			node_labels.append(self.G.nodes[x]["role"])
+			# nb, nb_list = self.get_neighborhood_of_x(x, receptive_field)
+			nb, nb_list = self.get_nodes_x_hops_away(x, receptive_field)
+
 			A_mean = self.get_adj_matrix(nb, nb_list, x, self.G.nodes[x], sample_size)
 			A_set.append(A_mean)
 		A_set = np.array(A_set)
-		self.conv_encoder.encode(A_set)
+		embeddings = self.conv_encoder.encode(A_set, emb_dim)
+		embeddings = pd.DataFrame(embeddings)
+		embeddings.columns = ["emb_"+str(i) for i in range(embeddings.shape[1])]
+		embeddings["node_id"] = node_ids
+		embeddings["role"] = node_labels
+		return embeddings
 
 
 	def get_neighborhood_of_x(self, x, receptive_field):
@@ -47,7 +59,41 @@ class LSME:
 				break
 			visited_nodes += nn_2
 			res.append(nn_2)
+		print(res)
 		return res, visited_nodes
+
+
+	def get_nodes_x_hops_away(self, node, receptive_field):
+		"""
+			This method will compute the number of neighbors x hops away from
+			a given node.
+		"""
+		node_dict = {node:0}
+		dist_dict = {0:[node]}
+		node_list = [node]
+		keep_going = True
+		visited_nodes = [node]
+		while keep_going:
+			n = node_list.pop(0)
+			nbs = self.G.neighbors(n)
+			for nb in nbs:
+				if (nb not in node_dict) and (nb != node):
+					node_list.append(nb)
+					dist_to_source = len(nx.shortest_path(self.G, source=node, target=nb)) - 1
+					node_dict[nb] = dist_to_source
+					if dist_to_source > receptive_field:
+						keep_going = False
+						break
+					visited_nodes.append(nb)
+					if dist_to_source not in dist_dict:
+						dist_dict[dist_to_source] = [nb]
+					else:
+						dist_dict[dist_to_source].append(nb)
+			if len(node_list) == 0:
+				keep_going = False
+		visited_nodes = list(set(visited_nodes))
+		return dist_dict, visited_nodes
+
 
 
 	def get_adj_matrix(self, nb, nb_list, node_id, node_obj, sample_size):
@@ -55,6 +101,7 @@ class LSME:
 			This method will create a sub-graph using the node list.
 			It will then create a adj matrix from that sub-graph.
 		"""
+		nb = list(nb.values())
 		H = self.G.subgraph(nb_list)
 		A_list = []
 		for ii in range(sample_size):
@@ -62,10 +109,11 @@ class LSME:
 			counter = 0
 			for i in nb:
 				random.shuffle(i)
-				degree_map = np.array(H.degree(i))
-				nodes = degree_map[:,0]
-				degrees = degree_map[:,1]
-				nodes = [x for _, x in sorted(zip(degrees, nodes), reverse=True)]
+				nodes = i[:]
+				# degree_map = np.array(H.degree(i))				
+				# nodes = degree_map[:,0]
+				# degrees = degree_map[:,1]
+				# nodes = [x for _, x in sorted(zip(degrees, nodes), reverse=True)]
 				for j in nodes:
 					node_map[j] = counter
 					counter += 1
@@ -73,10 +121,12 @@ class LSME:
 			A = nx.to_numpy_matrix(HH, nodelist=sorted(list(HH.nodes)))
 			A_list.append(A)
 		A_mean = np.mean(A_list, axis=0)
-		plt.imshow(A_mean)
-		plt.xticks([])
-		plt.yticks([])
-		# plt.show()
-		# exit(0)
-		plt.savefig("./test_folder/"+str(node_id)+".png", bbox_inches='tight')
+		for ii in range(len(A_list)):
+			plt.imshow(A_list[ii])
+			plt.xticks([])
+			plt.yticks([])
+			# plt.show()
+			# plt.savefig("./test_folder/"+str(node_id)+".png", bbox_inches='tight')
+			plt.savefig("./test_folder/"+str(ii)+".png", bbox_inches='tight')
+		exit(0)
 		return A_mean
--- LSME/conv_encoder.py
+++ LSME/conv_encoder.py
@@ -1,41 +1,40 @@
 
 import torch
 import torch.nn as nn
+import matplotlib.pyplot as plt
 from torchsummary import summary
 from torch.utils.data import DataLoader
 
 class Autoencoder(nn.Module):
-	def __init__(self):
+
+	def __init__(self, emb_dim, encoder_out_size, channel_size):
 		super(Autoencoder, self).__init__()
+		# Class variables
+		self.encoder_out_size = encoder_out_size
+		self.channel_size = channel_size
+		# Conv Encoder
 		self.encoder = nn.Sequential(
-			nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, stride=2, padding=1),
-			nn.ReLU(True),
-			nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=2, padding=1),
-			nn.ReLU(True),
-			nn.Conv2d(in_channels=16, out_channels=8, kernel_size=3, stride=2, padding=1),
-			nn.ReLU(True),
+			nn.Conv2d(in_channels=1, out_channels=self.channel_size, kernel_size=3, stride=1, padding=1),
+			nn.ReLU(inplace=True)
 		)
-
-		self.flatten = nn.Flatten()
-		self.fc_middle = nn.Linear(in_features=8*4*4, out_features=32)
-
-
+		# Dense Embedding Layer
+		self.embedding_input = nn.Flatten()
+		self.embedding_layer = nn.Linear(in_features=self.channel_size*self.encoder_out_size*self.encoder_out_size, out_features=emb_dim)
+		self.embedding_output = nn.Linear(in_features=emb_dim, out_features=self.channel_size*self.encoder_out_size*self.encoder_out_size)
+		# Conv Decoder
 		self.decoder = nn.Sequential(
-			nn.ConvTranspose2d(in_channels=8, out_channels=16, kernel_size=3, stride=2),  # 8x8x16
-			nn.ReLU(True),
-			nn.ConvTranspose2d(in_channels=16, out_channels=8, kernel_size=3, stride=2, output_padding=1),  # 15x15x8
-			nn.ReLU(True),
-			nn.ConvTranspose2d(in_channels=8, out_channels=1, kernel_size=3, stride=2, output_padding=1),  # 30x30x1
-			nn.Tanh()
+			nn.ConvTranspose2d(in_channels=self.channel_size, out_channels=1, kernel_size=3, stride=1, padding=1, output_padding=0),
+			nn.Sigmoid()
 		)
 
 
 	def forward(self, x):
 		encoded = self.encoder(x)
-		encoded = self.flatten(encoded)
-		encoded = self.fc_middle(encoded)
-		decoded = self.decoder(encoded.view(-1, 8, 4, 4))
-		return decoded
+		encoded = self.embedding_input(encoded)
+		embedding = self.embedding_layer(encoded)
+		encoded = self.Embedding_output(embedding)
+		decoded = self.decoder(encoded.view(-1, self.channel_size, self.encoder_out_size, self.encoder_out_size))
+		return [decoded, embedding]
 
 
 class Conv_Encoder:
@@ -43,23 +42,52 @@ class Conv_Encoder:
 	def __init__(self):
 		pass
 
-	def encode(self, data):
-		model = Autoencoder()
+	def encode(self, data, emb_dim):
+		
+		channel_size = 4
+		kernel_size = 3
+		padding = 1
+		stride = 1
 
+		# Pre-calculate the encoder output size
+		input_size = data.shape[1]
+		encoder_out_size = int(((input_size + 2*padding - kernel_size)/(stride)) + 1)
+
+		model = Autoencoder(emb_dim, encoder_out_size, channel_size)
 		data_tensor = torch.from_numpy(data).float()
-		data_loader = DataLoader(data_tensor, batch_size=32, shuffle=True)
-
+		data_loader = DataLoader(data_tensor, batch_size=1, shuffle=False)
 		optimizer = torch.optim.Adam(model.parameters())
-
-
 		num_epochs = 10
 		for epoch in range(num_epochs):
 			for batch_idx, data in enumerate(data_loader):
-				data = data.view(-1, 1, 28, 28)
+				data = data.view(-1, 1, input_size, input_size)
 				optimizer.zero_grad()
 				output = model(data)
-				loss = nn.MSELoss()(output, data)
+
+				loss = nn.MSELoss()(output[0], data)
 				loss.backward()
 				optimizer.step()
+		
+		# Compute Embeddings
+		embeddings = []
+		for batch_idx, data in enumerate(data_loader):
+			# xx = data.detach().cpu().numpy()
+			# plt.imshow(xx[0])
+			# plt.xticks([])
+			# plt.yticks([])
+			# plt.show()
 
-		exit(0)
+			data = data.view(-1, 1, input_size, input_size)
+			pred = model(data)
+			
+			# yy = pred[0].detach().cpu().numpy()
+			# plt.imshow(yy[0][0])
+			# plt.xticks([])
+			# plt.yticks([])
+			# plt.show()
+			
+			embedding =list(pred[1].detach().cpu().numpy().flatten())
+			embeddings.append(embedding)
+
+		return embeddings
+
